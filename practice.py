a34f3bca2646a1124fe654009afcdd95e949fefb
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2024-10-01 10:31:24 -0700

    initial algo practice

---
+++ practice.py
@@ -0,0 +1,54 @@
+# def calc_age(age):
+#     if age < 0:
+#         return "no negative age allowed"
+#     else:
+#         days = age * 365
+#         return days
+    
+# print(calc_age(-2))
+
+# def find_perimeter(length, width):
+#     rl = length * 2
+#     rw = width * 2
+#     return rl + rw
+
+# print(find_perimeter(6,7))
+
+#return the remainder from two numbers
+
+# def remainder(x,y):
+#     r = x % y
+#     return r
+
+# print(remainder(7,2))
+
+# return the next number from the integer passed
+
+# def addition(num):
+#     sum = num + 1
+#     return sum
+
+# print(addition(0))
+
+# power calculator
+
+# def power(voltage, current):
+#     p = voltage * current
+#     return p
+
+# print(power(230,10))
+
+#calculate exponent
+# def calculate_exponent(num, exp):
+#     c = num ** exp
+#     return c
+
+# print(calculate_exponent(5,5))
+
+#convert minutes to seconds
+
+# def minutes_to_seconds(minutes):
+#     t = minutes * 60
+#     return t
+
+# print(minutes_to_seconds(5))
