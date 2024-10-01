1c9410b7c5c7f201fa1c804bf4a90cd7eeade41f
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2024-10-01 13:04:51 -0700

    more unit tests

--- practice.py
+++ practice.py
@@ -1,32 +1,60 @@
 import unittest
 
 
+# class Solution:
+#     def convert(self, hours, minutes):
+#         c1 = hours * 3600
+#         c2 = minutes * 60
+#         return c1 + c2
+
+
+# class TestValue(unittest.TestCase):
+#     def test(self):
+#         example = Solution()
+#         self.assertEqual(example.convert(1,0), 3600)
+
+
+
+# class Result:
+#     def check_string(self,word):
+#         if type(word) == str:
+#             return True
+#         else:
+#             return False
+        
+# class TestString(unittest.TestCase):
+#     def test_word(self):
+#         example = Result()
+#         self.assertEqual(example.check_string(12), True)
+
+# class Solution:
+#     def addUp(self, num):
+#         count = 0
+#         for i in range(1, num+1):
+#             count = count + i
+#         return count
+        
+    
+# class TestValue(unittest.TestCase):
+#     def test(self):
+#         example = Solution()
+#         self.assertEqual(example.addUp(13), 91)    
+
 class Solution:
-    def convert(self, hours, minutes):
-        c1 = hours * 3600
-        c2 = minutes * 60
-        return c1 + c2
-
-
+    def sum_polygon(self, n):
+        count = (n-2) * 180
+        return count
+        
+    
 class TestValue(unittest.TestCase):
     def test(self):
         example = Solution()
-        self.assertEqual(example.convert(1,0), 3600)
+        self.assertEqual(example.sum_polygon(4), 360)  
 
-
-
-class Result:
-    def check_string(self,word):
-        if type(word) == str:
-            return True
-        else:
-            return False
-        
-class TestString(unittest.TestCase):
-    def test_word(self):
-        example = Result()
-        self.assertEqual(example.check_string(12), True)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
