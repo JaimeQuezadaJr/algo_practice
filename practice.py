0f1aeb45d1f3c728f892ad5056621535bd739d95
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2024-10-01 11:17:56 -0700

    unit test practice

--- practice.py
+++ practice.py
@@ -1,54 +1,32 @@
-# def calc_age(age):
-#     if age < 0:
-#         return "no negative age allowed"
-#     else:
-#         days = age * 365
-#         return days
-    
-# print(calc_age(-2))
+import unittest
 
-# def find_perimeter(length, width):
-#     rl = length * 2
-#     rw = width * 2
-#     return rl + rw
 
-# print(find_perimeter(6,7))
+class Solution:
+    def convert(self, hours, minutes):
+        c1 = hours * 3600
+        c2 = minutes * 60
+        return c1 + c2
 
-#return the remainder from two numbers
 
-# def remainder(x,y):
-#     r = x % y
-#     return r
+class TestValue(unittest.TestCase):
+    def test(self):
+        example = Solution()
+        self.assertEqual(example.convert(1,0), 3600)
 
-# print(remainder(7,2))
 
-# return the next number from the integer passed
 
-# def addition(num):
-#     sum = num + 1
-#     return sum
+class Result:
+    def check_string(self,word):
+        if type(word) == str:
+            return True
+        else:
+            return False
+        
+class TestString(unittest.TestCase):
+    def test_word(self):
+        example = Result()
+        self.assertEqual(example.check_string(12), True)
 
-# print(addition(0))
 
-# power calculator
-
-# def power(voltage, current):
-#     p = voltage * current
-#     return p
-
-# print(power(230,10))
-
-#calculate exponent
-# def calculate_exponent(num, exp):
-#     c = num ** exp
-#     return c
-
-# print(calculate_exponent(5,5))
-
-#convert minutes to seconds
-
-# def minutes_to_seconds(minutes):
-#     t = minutes * 60
-#     return t
-
-# print(minutes_to_seconds(5))
+if __name__ == '__main__':
+    unittest.main()
